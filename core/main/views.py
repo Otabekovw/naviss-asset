4d8a6d219f941bdb909a132ebb2aaaf14e7fb0a2
Otabekovw <sunikarol1113@example.com>
2025-08-03 17:31:46 +0600

    Add serializers and fix relative import

--- core/main/views.py
+++ core/main/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from .models import Partner, FAQ, News, Feedback
-from .serializers import  PartnerSerializer, FAQSerializer, NewsSerializer, FeedbackSerializer
+from .serializers import PartnerSerializer, FAQSerializer, NewsSerializer, FeedbackSerializer
 
 class PartnerAPIView(generics.ListAPIView):
     queryset = Partner.objects.all()
